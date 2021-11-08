6f02d7f5b4a03edb0fd4719ba3806e78b1b62f43
Leon Wisskirchen <hipwis@web.de>
2021-11-08 16:11:15 +0000

    asdf

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,18 @@
 class QuestionsController < ApplicationController
   def ask
   end
-  def GET
+
+  def answer
+
+  @you = params[:question]
+
+  @coach
+    if @you == "I am going to work right now!"
+      return @caoch = "Great!"
+    elsif @you.slice(-1) == "?"
+      return @coach = "Silly question, get dressed and go to work!"
+    else
+      return @coach = "I don't care, get dressed and go to work!"
+    end
   end
 end
